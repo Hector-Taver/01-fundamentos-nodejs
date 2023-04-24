f3a5737d243b0d4158f1824ad1edba33a043db7d
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-04-24 20:12:35 -0300

    Simulating a GET request and a POST request

--- src/server.js
+++ src/server.js
@@ -1,7 +1,32 @@
 import http from 'node:http'
 
+/*
+  HTTP Methods => GET, POST, PUT, PATCH, DELETE
+
+  GET => Buscar uma recurso do back end
+
+  POST => Criar uma recurso no back end
+
+  PUT => Atualizar um recurso no back end
+
+  PATCH => Atualizar uma informação específica de um recurso no back end
+
+  DELETE => Deletar um recurso do back end
+*/
+
 const server = http.createServer((req, res) => {
-  return res.end('Hello World')
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    // Early return
+    return res.end('Listagem de usuários.')
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return res.end('Criação de usuários.')
+  }
+
+  return res.end('Hello Ignite!')
 })
 
 // localhost:3333
